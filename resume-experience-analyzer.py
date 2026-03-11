e6d015822ecb60bd7fa46d13faf3afebc5ae5646
Bijena Dhewaju <bijenadhewaju181@gmail.com>
2026-03-11 11:43:05 +0545

    Feature-3 added  2026-03-11

---
+++ resume-experience-analyzer.py
@@ -0,0 +1,11 @@
+name=input("enter your name ")
+experience_years = int(input("enter your years of experience "))
+if experience_years < 1:
+    role="Intern or Trainee"
+elif experience_years < 3:
+    role="Junior Developer"
+elif experience_years < 5:
+    role="Mid-level Developer"
+else:
+    role="Senior Developer"
+print(f"{name}, based on your {experience_years} years of experience, you are eligible for the role of {role}.")
